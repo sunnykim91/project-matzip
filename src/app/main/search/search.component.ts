69578864ff85c92aca3fa3c1dde32a5da720e8da
sunnykim91 <tjsgnl2002@gmail.com>
2019-06-27 10:34:06 +0900

    data list add

--- src/app/main/search/search.component.ts
+++ src/app/main/search/search.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Matzips } from '../../matzips.interface'
+import { matzipList } from '../../matzip-data'
 
 @Component({
   selector: 'app-search',
   template: `
-    <p>
-      search works!
-    </p>
+    <div *ngFor="let list of matzipList">
+      {{list.name}}
+      {{list.menu}}
+      {{list.address}}
+    </div>
   `,
   styles: []
 })
 export class SearchComponent implements OnInit {
+  matzipList: Matzips[] = matzipList;
 
   constructor() { }
 
