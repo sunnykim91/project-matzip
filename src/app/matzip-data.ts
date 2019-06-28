42b5f9c7191023047d833586be15c4cfbae6aa1c
dearjsmc4 <dearjsmc4@gmail.com>
2019-06-28 14:04:32 +0900

    Info window style & setCenter ongoing

--- src/app/matzip-data.ts
+++ src/app/matzip-data.ts
@@ -47,5 +47,311 @@ export const matzipList: Matzips[] =
     "address": "서울 성동구 홍익동 431",
     "completed": false,
     "wishable": false
+  },
+  {
+    "id": 6,
+    "broadcastingname": "수요미식회",
+    "name": "대흥식육점",
+    "menu": "스테이크",
+    "address": "경상북도 문경시 문경읍 하리 206-6",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 7,
+    "broadcastingname": "수요미식회",
+    "name": "서동한우 부여본점",
+    "menu": "한우",
+    "address": "충청남도 부여군 부여읍 관북리 118-2",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 8,
+    "broadcastingname": "수요미식회",
+    "name": "통일집",
+    "menu": "한우등심",
+    "address": "서울 중구 을지로3가 202",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 9,
+    "broadcastingname": "수요미식회",
+    "name": "소호정",
+    "menu": "안동국시",
+    "address": "서울 서초구 양재동 392-11",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 10,
+    "broadcastingname": "수요미식회",
+    "name": "찬양집",
+    "menu": "칼국수",
+    "address": "서울 종로구 돈의동 27",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 11,
+    "broadcastingname": "수요미식회",
+    "name": "명동교자 본점",
+    "menu": "칼국수",
+    "address": "서울 중구 명동2가 25-2",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 12,
+    "broadcastingname": "수요미식회",
+    "name": "국시집",
+    "menu": "칼국수",
+    "address": "서울 성북구 성북동1가 9",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 13,
+    "broadcastingname": "수요미식회",
+    "name": "해밀손칼국수",
+    "menu": "칼국수",
+    "address": "서울 관악구 청림동 16-41",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 14,
+    "broadcastingname": "수요미식회",
+    "name": "개성집",
+    "menu": "만두 #국수",
+    "address": "경기도 남양주시 조안면 송촌1리 792-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 15,
+    "broadcastingname": "수요미식회",
+    "name": "베트남 고향식당",
+    "menu": "베트남음식",
+    "address": "경기도 안산시 단원구 원곡동 787-5",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 16,
+    "broadcastingname": "수요미식회",
+    "name": "할매국수",
+    "menu": "잔치국수",
+    "address": "경상북도 포항시 남구 구룡포읍 구룡포리 963-120",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 17,
+    "broadcastingname": "수요미식회",
+    "name": "대성갈비",
+    "menu": "돼지갈비",
+    "address": "서울 성동구 성수동1가 668-21",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 18,
+    "broadcastingname": "수요미식회",
+    "name": "조박집",
+    "menu": "돼지갈비",
+    "address": "서울 마포구 용강동 40-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 19,
+    "broadcastingname": "수요미식회",
+    "name": "목포산꽃게",
+    "menu": "꽃게찜",
+    "address": "서울 성동구 마장동 767-41",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 20,
+    "broadcastingname": "수요미식회",
+    "name": "광양삼대불고기집",
+    "menu": "광양불고기",
+    "address": "전라남도 광양시 광양읍 칠성리 959-11",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 21,
+    "broadcastingname": "수요미식회",
+    "name": "한일관",
+    "menu": "불고기",
+    "address": "서울 강남구 신사동 619-4",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 22,
+    "broadcastingname": "수요미식회",
+    "name": "언양 기와집 불고기",
+    "menu": "언양불고기",
+    "address": "울산 울주군 언양읍 서부리 11-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 23,
+    "broadcastingname": "수요미식회",
+    "name": "쿄 베이커리",
+    "menu": "빵집",
+    "address": "서울 마포구 상수동 317-7",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 24,
+    "broadcastingname": "수요미식회",
+    "name": "폴앤폴리나",
+    "menu": "빵집",
+    "address": "서울 마포구 서교동 343-11 1층",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 25,
+    "broadcastingname": "수요미식회",
+    "name": "리치몬드",
+    "menu": "빵집",
+    "address": "서울 마포구 성산1동 114-5",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 26,
+    "broadcastingname": "수요미식회",
+    "name": "송학낙지회관",
+    "menu": "낙지",
+    "address": "전라남도 목포시 상동 971-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 27,
+    "broadcastingname": "수요미식회",
+    "name": "제일회식당",
+    "menu": "낙지",
+    "address": "전라남도 무안군 망운면 목서리 636",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 28,
+    "broadcastingname": "수요미식회",
+    "name": "유림낙지",
+    "menu": "낙지볶음",
+    "address": "서울 종로구 서린동 129-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 29,
+    "broadcastingname": "수요미식회",
+    "name": "광주식당",
+    "menu": "청국장",
+    "address": "서울 동대문구 청량리동 773",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 30,
+    "broadcastingname": "수요미식회",
+    "name": "임금님쌀밥집",
+    "menu": "한정식",
+    "address": "경기도 이천시 신둔면 수광리 152-9",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 31,
+    "broadcastingname": "수요미식회",
+    "name": "툇마루밥상",
+    "menu": "한정식",
+    "address": "서울 송파구 문정1동 46-11",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 32,
+    "broadcastingname": "수요미식회",
+    "name": "현고대 닭발",
+    "menu": "닭발",
+    "address": "서울 동대문구 제기동 67-6",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 33,
+    "broadcastingname": "수요미식회",
+    "name": "풍년 닭도리탕",
+    "menu": "닭볶음탕",
+    "address": "서울 중구 서소문동 120-12",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 34,
+    "broadcastingname": "수요미식회",
+    "name": "동기간",
+    "menu": "백숙",
+    "address": "경기도 가평군 가평읍 개곡리 487",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 35,
+    "broadcastingname": "수요미식회",
+    "name": "낙선재",
+    "menu": "한정식",
+    "address": "경기도 광주 중부면 불당리 194-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 36,
+    "broadcastingname": "수요미식회",
+    "name": "조선옥",
+    "menu": "소갈비",
+    "address": "서울 중구 을지로3가 229-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 37,
+    "broadcastingname": "수요미식회",
+    "name": "삼도갈비",
+    "menu": "평양냉면 #갈비",
+    "address": "경기도 부천시 원미구 상동 573-1",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 38,
+    "broadcastingname": "수요미식회",
+    "name": "벽제갈비",
+    "menu": "한우",
+    "address": "서울 송파구 방이동 205-8",
+    "completed": false,
+    "wishable": false
+  },
+  {
+    "id": 39,
+    "broadcastingname": "수요미식회",
+    "name": "우동 카덴",
+    "menu": "우동",
+    "address": "서울 마포구 서교동 391-5",
+    "completed": false,
+    "wishable": false
   }
 ]
